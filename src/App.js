3dcbc35edc3eb77e2a4f58e4b598b9890ddd8efe
hmesfin <hamelmesfin@gmail.com>
2024-03-13 12:37:57 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, there! I'm a React app running on AWS Amplify. I was deployed using the AWS Amplify Console.
         </p>
         <a
           className="App-link"
